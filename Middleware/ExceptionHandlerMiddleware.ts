13a2eb57ccaf14697cc28e2655690a2ebb558817
nyyakko <nyakonyns@gmail.com>
2025-09-27 20:41:20 -0300

    return code instead

--- Middleware/ExceptionHandlerMiddleware.ts
+++ Middleware/ExceptionHandlerMiddleware.ts
@@ -6,7 +6,7 @@ export function the()
 {
     return (error: HttpException, _request: Request, response: Response, next: NextFunction) => {
         if (error) {
-            return response.status(error.status || 500).json({ error: error.code });
+            return response.status(error.status || 500).json({ code: error.code });
         }
         return next();
     };
