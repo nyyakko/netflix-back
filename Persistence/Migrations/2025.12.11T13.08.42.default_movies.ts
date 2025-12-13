11ea3fd5fe4b5f17f339f654912731efa385b67f
nyyakko <nyakonyns@gmail.com>
2025-12-13 17:58:28 -0300

    remove genre fetch for now

--- Persistence/Migrations/2025.12.11T13.08.42.default_movies.ts
+++ Persistence/Migrations/2025.12.11T13.08.42.default_movies.ts
@@ -8,46 +8,42 @@ const MOVIEDB_BASE_URL = 'https://api.themoviedb.org';
 
 export async function up({ context: sequelize }: MigrationParams<Sequelize<PostgresDialect>>)
 {
-    const originalsResponse = await fetch(`${MOVIEDB_BASE_URL}/3/discover/tv?api_key=${process.env.MOVIEDB_API_KEY}&language=pt-br&with_networks=213`);
+    const originalsResponse = await Promise.all([
+        fetch(`${MOVIEDB_BASE_URL}/3/discover/tv?api_key=${process.env.MOVIEDB_API_KEY}&language=pt-br&page=1&with_networks=213`),
+        fetch(`${MOVIEDB_BASE_URL}/3/discover/tv?api_key=${process.env.MOVIEDB_API_KEY}&language=pt-br&page=2&with_networks=213`),
+        fetch(`${MOVIEDB_BASE_URL}/3/discover/tv?api_key=${process.env.MOVIEDB_API_KEY}&language=pt-br&page=3&with_networks=213`),
+    ]);
+    const originals = await Promise.all(originalsResponse.map(response => response.json())) as any[];
 
-    const originals = await originalsResponse.json() as any;
-    let originalsGenres: number[][] = [];
-
-    await sequelize.queryInterface.bulkInsert('movies', originals.results.map((entry: any) => {
-        originalsGenres.push(entry.genre_ids);
-        return {
-            title: entry.name,
-            synopsis: entry.overview,
-            rating: entry.vote_average,
-            releaseDate: new Date(entry.first_air_date),
-            posterPath: `https://image.tmdb.org/t/p/w500${entry.poster_path}`,
-            backdropPath: `https://image.tmdb.org/t/p/w1280${entry.backdrop_path}`,
-            popularity: entry.popularity,
-            original: true
-        }
-    }));
-
-    for (let i = 0; i < originalsGenres.length; i += 1) {
-        await sequelize.queryInterface.bulkInsert('movie_genres', originalsGenres[i]!.map(genre => {
-            return { movieId: i+1, genreId: genre }
-        }));
-    }
+    await sequelize.queryInterface.bulkInsert('movies',
+        originals
+            .map((entry: any) => entry.results).flat()
+            .map((entry: any) => {
+                return {
+                    title: entry.name,
+                    synopsis: entry.overview,
+                    rating: entry.vote_average,
+                    releaseDate: new Date(entry.first_air_date),
+                    posterPath: `https://image.tmdb.org/t/p/w500${entry.poster_path}`,
+                    backdropPath: `https://image.tmdb.org/t/p/w1280${entry.backdrop_path}`,
+                    popularity: entry.popularity,
+                    original: true
+                }
+            })
+    );
 
     const nonOriginalsResponse = await Promise.all([
         fetch(`${MOVIEDB_BASE_URL}/3/discover/tv?api_key=${process.env.MOVIEDB_API_KEY}&language=pt-br&page=1`),
         fetch(`${MOVIEDB_BASE_URL}/3/discover/tv?api_key=${process.env.MOVIEDB_API_KEY}&language=pt-br&page=2`),
         fetch(`${MOVIEDB_BASE_URL}/3/discover/tv?api_key=${process.env.MOVIEDB_API_KEY}&language=pt-br&page=3`),
-    ])
-
+    ]);
     const nonOriginals = await Promise.all(nonOriginalsResponse.map(response => response.json())) as any[];
-    let nonOriginalsGenres: number[][] = [];
 
     await sequelize.queryInterface.bulkInsert('movies',
         nonOriginals
             .map((entry: any) => entry.results).flat()
-            .filter((entry: any) => !originals.results.find((original: any) => original.name == entry.name))
+            .filter((entry: any) => !originals.map((entry: any) => entry.results).flat().find((original: any) => original.name == entry.name))
             .map((entry: any) => {
-                nonOriginalsGenres.push(entry.genre_ids);
                 return {
                     title: entry.name,
                     synopsis: entry.overview,
@@ -60,12 +56,6 @@ export async function up({ context: sequelize }: MigrationParams<Sequelize<Postg
                 };
             })
     );
-
-    for (let i = 0; i < nonOriginalsGenres.length; i += 1) {
-        await sequelize.queryInterface.bulkInsert('movie_genres', nonOriginalsGenres[i]!.map(genre => {
-            return { movieId: i+1, genreId: genre }
-        }));
-    }
 }
 
 export async function down({ context: sequelize }: MigrationParams<Sequelize<PostgresDialect>>)
